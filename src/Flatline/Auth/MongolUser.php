fcd28a7b15367f5b3fff645fc98894d51bfc51c1
Luciano Longo <flatline@home>
2013-01-22 18:54:33 -0300

    Added __isset as this is lacking in Laravel 4 yet

--- src/Flatline/Auth/MongolUser.php
+++ src/Flatline/Auth/MongolUser.php
@@ -13,4 +13,15 @@ class MongolUser extends GenericUser {
     {
         return $this->attributes['_id'];
     }
+
+    /**
+     * Dynamically check if the user has the given attribute.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function __isset($key)
+    {
+           return isset($this->attributes[$key]);
+    }
 }
